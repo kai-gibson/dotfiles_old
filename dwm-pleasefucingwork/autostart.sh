e7037461c6f9e50f088bc40f175463771e8ba52c
k-gs <s3815267@student.rmit.edu.au>
2022-01-10 13:36:44 +1100

    kai fork222d

---
+++ dwm-pleasefucingwork/autostart.sh
@@ -0,0 +1,10 @@
+#! /bin/bash
+
+dwmblocks &
+nitrogen --restore &
+picom &
+cbatticon -u 20 -i standard -c "poweroff" -l 15 -r 5 &
+nextcloud --background &
+nm-applet --no-agent &
+brave &
+logseq &
