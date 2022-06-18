4539d083d24698ee8f2da6d3a108e234783721d6
Kai Gibson <kai.gib@tuta.io>
2022-06-18 22:22:53 +1000

    fish update

--- fish/config.fish
+++ fish/config.fish
@@ -11,8 +11,84 @@ alias hibernate="systemctl hibernate"
 alias pac="paru"
 alias bright="xrandr --output eDP-1 --brightness"
 alias screenshot="flameshot launcher"
+alias lf=lfub
 
 # Automatically start a tmux session on booting fish 
 # (script has an if handler for tmux already existing and throws a silent error)
 
 sh ~/.dotfiles/scripts/tmuxStart.sh
+export EDITOR='nvim'
+
+# This is the list for lf icons:
+export LF_ICONS="di=📁:\
+fi=:\
+tw=🤝:\
+ow=📂:\
+ln=:\
+or=❌:\
+ex=:⌖\
+*.txt=:\
+*.mom=:\
+*.me=:\
+*.ms=:\
+*.png=:\
+*.webp=:\
+*.ico=:\
+*.jpg=:\
+*.jpe=:\
+*.jpeg=:\
+*.gif=:\
+*.svg=:\
+*.tif=:\
+*.tiff=:\
+*.xcf=🖌:\
+*.html=🌎:\
+*.xml=📰:\
+*.gpg=🔒:\
+*.css=🎨:\
+*.pdf=:\
+*.djvu=:\
+*.epub=:\
+*.csv=:\
+*.xlsx=:\
+*.tex=📜:\
+*.md=📜:\
+*.r=📜:\
+*.R=📜:\
+*.rmd=📜:\
+*.Rmd=📜:\
+*.m=📜:\
+*.mp3=🎵:\
+*.opus=🎵:\
+*.ogg=🎵:\
+*.m4a=🎵:\
+*.flac=🎵:\
+*.wav=🎵:\
+*.mkv=:\
+*.mp4=🎵:\
+*.webm=:\
+*.mpeg=:\
+*.avi=:\
+*.mov=:\
+*.mpg=:\
+*.wmv=:\
+*.m4b=:\
+*.flv=:\
+*.zip=📦:\
+*.rar=📦:\
+*.7z=📦:\
+*.tar.gz=📦:\
+*.1=ℹ:\
+*.nfo=ℹ:\
+*.info=ℹ:\
+*.log=📙:\
+*.iso=📀:\
+*.img=📀:\
+*.bib=🎓:\
+*.ged=👪:\
+*.part=💔:\
+*.torrent=🔽:\
+*.jar=♨:\
+*.java=♨:\
+"
+
